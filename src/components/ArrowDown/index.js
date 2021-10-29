ac055c49467937dcbefd34423f1965dd04052448
Emerson-lab <trinddade29@gmail.com>
2021-10-28 23:15:08 -0300

    Add CSS in ArrowDown with animation

---
+++ src/components/ArrowDown/index.js
@@ -0,0 +1,14 @@
+import "./style.css";
+
+function ArrowDown(currentPlayer = 1) {
+  return /*html*/ `
+    <img 
+      class="arrow-down" 
+      data-currentPlayer = "${currentPlayer}"
+      src="/images/IconArrowDown.png" 
+      alt="Seta para baixo" 
+    />
+  `;
+}
+
+export default ArrowDown;
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,18 +1,20 @@
-import "./style.css"
-import PlayerName from "../../components/PlayerName"
+import "./style.css";
+import PlayerName from "../../components/PlayerName";
 import VsPlayer from "../../components/VsPlayer";
 import PlayerScore from "../../components/PlayerScore";
+import ArrowDown from "../../components/ArrowDown";
 
 function ScoreBoard() {
-    return /*html*/`
+  return /*html*/ `
       <header class="score-board">
-        ${PlayerName("player1")}
+        ${ArrowDown(2)}
+        ${PlayerName("player1")} 
         ${PlayerScore(1)}
         ${VsPlayer()}
         ${PlayerScore(3)}
         ${PlayerName("player2")}
       </header>
-    `
+    `;
 }
 
 export default ScoreBoard;
