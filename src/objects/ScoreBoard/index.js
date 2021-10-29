e55413480afeeb5be0e66f4c32f87bf81b53ca32
Emerson-lab <suporte@portasabertas.org.br>
2021-10-28 21:55:43 -0300

    Create component VsPlayer

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,10 +1,12 @@
 import "./style.css"
 import PlayerName from "../../components/PlayerName"
+import VsPlayer from "../../components/VsPlayer";
 
 function ScoreBoard() {
     return /*html*/`
       <header class="score-board">
         ${PlayerName("player1")}
+        ${VsPlayer()}
         ${PlayerName("player2")}
       </header>
     `
