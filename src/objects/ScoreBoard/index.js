c9d4f0b1036c4570007626a9b2220c80bcff870e
Emerson-lab <trinddade29@gmail.com>
2021-10-28 22:42:58 -0300

    Create Score css

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,12 +1,15 @@
 import "./style.css"
 import PlayerName from "../../components/PlayerName"
 import VsPlayer from "../../components/VsPlayer";
+import PlayerScore from "../../components/PlayerScore";
 
 function ScoreBoard() {
     return /*html*/`
       <header class="score-board">
         ${PlayerName("player1")}
+        ${PlayerScore(1)}
         ${VsPlayer()}
+        ${PlayerScore(3)}
         ${PlayerName("player2")}
       </header>
     `
