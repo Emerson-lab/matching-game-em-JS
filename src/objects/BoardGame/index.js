f14147d468f4188d17c1d1e76be05a14f519dde9
trinddade29@gmail.com <trinddade29@gmail.com>
2021-11-19 11:48:53 -0300

    Create Score dinymic

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -2,15 +2,20 @@ import "./style.css";
 import CardFrontBack from "../../components/CardFrontBack";
 import cards from "./data.js";
 
-function BoardGame(amountCards) {
- 
+function BoardGame() {
+  window.boardGame = {}
+  window.boardGame.handleClick = () => {
+    const $boardGame = document.querySelector(".board-game")
+
+    const $cardsActive = $boardGame.querySelector(".card-front-back.-active")
+  }
   const htmlCardsList = cards.map((card) =>
     CardFrontBack(card.icon, card.altIcon)
   );
   const $htmlCards = htmlCardsList.join("");
 
   return /*html*/ `
-    <section class="board-game">
+    <section class="board-game" onClick="boardGame.handleClick()">
       ${$htmlCards}
     </section>
   `;
